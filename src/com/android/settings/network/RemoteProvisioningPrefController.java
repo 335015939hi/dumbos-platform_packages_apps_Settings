GrapheneOS/platform_packages_apps_Settings

remote key provisioning server setting
Co-authored-by: flawedworld <flawedworld@flawed.world>
Signed-off-by: r3g_5z <june@girlboss.ceo>

src/com/android/settings/network/RemoteProvisioningPrefController.java
@@ -1,0 +1,37 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.network;
+
+import android.content.Context;
+import android.ext.settings.ExtSettings;
+import android.ext.settings.RemoteProvisioningConstants;
+
+import com.android.settings.R;
+import com.android.settings.ext.IntSettingPrefController;
+
+public class RemoteProvisioningPrefController extends IntSettingPrefController {
+
+    public RemoteProvisioningPrefController(Context ctx, String key) {
+        super(ctx, key, ExtSettings.REMOTE_PROVISIONING_SERVER);
+    }
+
+    @Override
+    protected void getEntries(Entries entries) {
+        entries.add(R.string.remote_provisioning_enabled_grapheneos_proxy, RemoteProvisioningConstants.GRAPHENEOS_PROXY);
+        entries.add(R.string.remote_provisioning_enabled_google_server, RemoteProvisioningConstants.STANDARD_SERVER);
+    }
+}

335015939hi/dumbos-platform_packages_apps_Settings

src/com/android/settings/network/RemoteProvisioningPrefController.java
@@ -1,0 +1,36 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.network;
+
+import android.content.Context;
+import android.ext.settings.RemoteKeyProvisioningSettings;
+
+import com.android.settings.R;
+import com.android.settings.ext.IntSettingPrefController;
+
+public class RemoteProvisioningPrefController extends IntSettingPrefController {
+
+    public RemoteProvisioningPrefController(Context ctx, String key) {
+        super(ctx, key, RemoteKeyProvisioningSettings.SERVER_SETTING);
+    }
+
+    @Override
+    protected void getEntries(Entries entries) {
+        entries.add(R.string.remote_provisioning_enabled_grapheneos_proxy, RemoteKeyProvisioningSettings.GRAPHENEOS_PROXY);
+        entries.add(R.string.remote_provisioning_enabled_google_server, RemoteKeyProvisioningSettings.STANDARD_SERVER);
+    }
+}