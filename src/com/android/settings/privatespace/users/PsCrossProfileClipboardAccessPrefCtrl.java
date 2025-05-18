ffc30123214d72dc97c2beba845b2443f8783b4d
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-05-18 16:41:53 +0000

    fixup! fixup! Settings to toggle cross-profile shared clipboard behavior

--- src/com/android/settings/privatespace/users/PsCrossProfileClipboardAccessPrefCtrl.java
+++ src/com/android/settings/privatespace/users/PsCrossProfileClipboardAccessPrefCtrl.java
@@ -3,8 +3,11 @@ package com.android.settings.privatespace.users;
 import android.content.Context;
 import android.ext.settings.IntSetting;
 
+import androidx.preference.PreferenceScreen;
+
 import com.android.settings.R;
 import com.android.settings.ext.IntSettingPrefController;
+import com.android.settings.ext.RadioButtonPickerFragment2;
 import com.android.settings.privatespace.PrivateSpaceMaintainer;
 
 import static android.ext.settings.CrossProfileClipboardAccessSettings.*;
@@ -43,5 +46,10 @@ public class PsCrossProfileClipboardAccessPrefCtrl extends IntSettingPrefControl
         entries.add(R.string.cross_profile_clipboard_access_disallow_title,
                 R.string.cross_profile_clipboard_access_disallow_summary,
                 BLOCK);
+    }
+
+    @Override
+    public void addPrefsBeforeList(RadioButtonPickerFragment2 fragment, PreferenceScreen screen) {
+        addFooterPreference(screen, R.string.cross_profile_clipboard_access_footer);
     }
 }
